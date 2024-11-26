d49eeacfd9b83a6b403ba901b31f2b9923801422
S-Skaggs <s-skaggs@outlook.com>
2024-11-26 06:49:34 -0500

    Edited the app component
    Edited the app component to demo Signal.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,57 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, computed, effect } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <div class="centered">
+      <p>Count: {{ count() }}</p>
+      <p>Double Count: {{ doubleCount() }}</p>
+      <button class="leftButton" (click)="decrement()">Decrement</button>
+      <button class="rightButton" (click)="increment()">Increment</button>
+    </div>
+  `,
+  styles: `
+    .centered {
+      border: solid .1rem black;
+      padding: 0 0 .5rem .8rem;
+      width: 15rem;
+      margin: 5rem auto;
+    }
+
+    .leftButton {
+      position: relative;
+      left: .8rem;
+    }
+
+    .rightButton {
+      position: relative;
+      left: 2rem;
+    }
+  `
 })
 export class AppComponent {
-  title = 'simple-signals-demo';
+  title = 'test-angular-signals-app';
+
+  // Create a signal
+  count = signal(0);
+
+  // Create a computed signal
+  doubleCount = computed(() => this.count() * 2);
+
+  constructor() {
+    // Create an effect
+    effect(() => {
+      console.log(`Count is: ${this.count()}`);
+    });
+  }
+
+  increment() {
+    this.count.set(this.count() + 1);
+  }
+
+  decrement() {
+    this.count.set(this.count() - 1);
+  }
 }
